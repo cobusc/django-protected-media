980b865c17023e1395962e0ea821223e8d6d496f
cobusc <cobus.carstens@gmail.com>
2017-05-15 13:42:18 +0200

    Cleanup

--- protected_media/models.py
+++ protected_media/models.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.files.storage import FileSystemStorage, abspathu
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 from .settings import PROTECTED_MEDIA_ROOT, PROTECTED_MEDIA_URL
-
-
-# Create your models here.
-from django.utils.functional import cached_property
 
 
 class ProtectedFileSystemStorage(FileSystemStorage):
@@ -21,21 +17,6 @@ class ProtectedFileSystemStorage(FileSystemStorage):
         kwargs["location"] = PROTECTED_MEDIA_ROOT
         kwargs["base_url"] = PROTECTED_MEDIA_URL
         super(ProtectedFileSystemStorage, self).__init__(*args, **kwargs)
-
-#    @cached_property
-#    def base_location(self):
-#        return PROTECTED_MEDIA_ROOT
-#
-#    @cached_property
-#    def location(self):
-#        return abspathu(self.base_location)
-#
-#    @cached_property
-#    def base_url(self):
-#        result = PROTECTED_MEDIA_URL
-#        if not result.endswith('/'):
-#            result += '/'
-#        return result
 
 
 class ProtectedFileField(models.FileField):
